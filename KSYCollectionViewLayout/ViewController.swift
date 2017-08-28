837e2a819409bb8bfb000b77abc6464748142d64
huangdaxia <527548875@qq.com>
2017-08-28 16:07:36 +0800

    Add test demo.

--- KSYCollectionViewLayout/ViewController.swift
+++ KSYCollectionViewLayout/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var dataCount = 20
+    var columnCount = 2
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = KSYCollectionViewLayout()
+        layout.delegate = self
+        let collect = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collect.backgroundColor = UIColor.white
+        collect.delegate = self
+        collect.dataSource = self
+        collect.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Identifier")
+        
+        return collect
+    } ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(collectionView)
+        
+        collectionView.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    //MARK: UICollectionViewDelegate
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
+    //MARK: UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Identifier", for: indexPath)
+        cell.contentView.backgroundColor = [UIColor.blue, UIColor.red, UIColor.yellow][indexPath.row % 3]
+        cell.contentView.clipsToBounds = true
+        cell.contentView.layer.cornerRadius = 5
+        
+        return cell
+    }
+    
+}
+
+extension ViewController: KSYCollectionViewLayoutDelegate {
+    func numberOfColumn(in collectionView: UICollectionView) -> Int {
+        return columnCount
+    }
+    func collectionView(_ collectionView: UICollectionView, layout: KSYCollectionViewLayout, heightForItemAt indexPath: IndexPath) -> CGFloat {
+        let height = 200 + arc4random() % 100
+        
+        return CGFloat(height)
+    }
+}
